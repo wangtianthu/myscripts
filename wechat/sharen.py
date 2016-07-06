951db9aeb15803e9fdf6cbbe44dfba2339c1d023
Tian Wang <wangtian@twitter.com>
2016-07-05 17:50:33 -0700

    fix

--- wechat/sharen.py
+++ wechat/sharen.py
@@ -1,13 +1,21 @@
 import random
 import sys
 import sets
+import math
 
 def weighted_pick(count):
   ratio = 5.0  # the ratio of probability of killing first over killing the last
-  raw_weight = [ratio - (ratio - 1.0) * i / 499 for i in range(500)]
+  base = -500.0 / math.log(1.0 / ratio)
+  raw_weight = []
+  for i in range(500):
+    weight = ratio * math.exp(-i / base)
+    raw_weight += [ weight ]
   total = sum(raw_weight)
   weight = [w * 1.0 / total for w in raw_weight]  # normalize
 
+  # print "weight len = %d: %s" % (len(weight), weight)
+
+  random.seed()
   results = sets.Set()
   while len(results) < count:
     r = random.random() - weight[0]
@@ -23,11 +31,25 @@ to_die = weighted_pick(int(sys.argv[1]))
 to_die.sort()
 
 last=-1
+index=1
+output=[]
 for i in to_die:
-  print "count %3d ... kill %3d" % (i - last, i + 1)
+  diff = i - last
+  scr = diff / 7
+  output += [ "[%2d] count %3d ... ( %s x+ %d ) ... kill %3d" % (index, diff, " " if scr == 0 else scr, diff % 7, i + 1) ]
   last = i
+  index += 1
 
-# No longer need to display in columnar style, WeChat has changed
+output += [""] * (3 - len(output) % 3)
+
+lines = len(output) / 3 
+for i in range(lines):
+  print "%s\t\t%s\t\t%s" % (output[i], output[i + lines], output[i + lines*2])
+  if i % 3 == 2:
+    print
+print "\n\n"
+
+  
 # items = [(i/4, i%4) for i in to_die]
 # idx = 0
 # for i in range(0,125):
